207b33174c2e50ac414bde95abf2ad419418f57f
Jefferson <gentbliss@gmail.com>
2025-11-02 21:29:28 +0000

    Add files via upload

---
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        # A helpful method for displaying the object in the shell/admin
+        return f"{self.title} by {self.author}"
