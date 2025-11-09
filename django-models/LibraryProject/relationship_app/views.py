8a219eb73680e171e1dba234f26a640c8e003aac
Jefferson <gentbliss@gmail.com>
2025-11-09 18:29:51 +0000

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
 
 
 # Function-Based View
 def list_books(request):
-    books = Book.objects.all()
+    books = Book.objects.all()  # exact pattern expected
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
