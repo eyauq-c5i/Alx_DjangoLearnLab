088fa6ccaec96b1e7d50adcd46e8717b13ff0e3a
Jefferson <gentbliss@gmail.com>
2025-12-07 09:36:42 +0000

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -30,9 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Third-party apps
     'rest_framework',
+    'taggit',
 
     # Local apps
     'blog',
@@ -113,4 +112,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
