61bb9d7033fcd3a8b316f1a329acd4801ebc4ae7
Jefferson <gentbliss@gmail.com>
2025-12-13 21:36:00 +0000

    Refactor serializers to use get_user_model() directly

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,14 +2,12 @@ from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = [
             'id',
             'username',
@@ -19,7 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
@@ -55,7 +53,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     following_count = serializers.SerializerMethodField()
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = [
             'id',
             'username',
